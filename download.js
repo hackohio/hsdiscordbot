8723d1b89e4ec85193a6fc3362d9d2c96c96604a
thomasdw33 <56744083+thomasdw33@users.noreply.github.com>
2021-02-24 14:14:56 -0500

    File download updated

--- download.js
+++ download.js
@@ -1,4 +1,7 @@
 const config = require("./config.json");
+const fs = require('fs');
+//const { google } = require('googleapis');
+
 
 var fileId = config.fileID;
 var dest = fs.createWriteStream('~/hsdiscordbot/formresponses.csv');
