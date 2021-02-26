045a9012a44110bf1de048a53e710d4fed3cdcb7
egolfc <cae5511@gmail.com>
2021-02-26 13:11:44 -0500

    download file function

--- download.js
+++ download.js
@@ -1,19 +1,21 @@
 const config = require("./config.json");
-const fs = require('fs');
-const { google } = require('googleapis');
+const fs = require("fs");
+const { google } = require("googleapis");
 const readline = require("readline");
 
-
 // If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly', 'https://www.googleapis.com/auth/drive.readonly'];
+const SCOPES = [
+  "https://www.googleapis.com/auth/drive.metadata.readonly",
+  "https://www.googleapis.com/auth/drive.readonly",
+];
 // The file token.json stores the user's access and refresh tokens, and is
 // created automatically when the authorization flow completes for the first
 // time.
-const TOKEN_PATH = 'token.json';
+const TOKEN_PATH = "token.json";
 
 // Load client secrets from a local file.
-fs.readFile('credentials.json', (err, content) => {
-  if (err) return console.log('Error loading client secret file:', err);
+fs.readFile("credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
   // Authorize a client with credentials, then call the Google Drive API.
   authorize(JSON.parse(content), listFiles);
   authorize(JSON.parse(content), downloadFile);
@@ -26,9 +28,12 @@ fs.readFile('credentials.json', (err, content) => {
  * @param {function} callback The callback to call with the authorized client.
  */
 function authorize(credentials, callback) {
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
   const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
@@ -46,23 +51,23 @@ function authorize(credentials, callback) {
  */
 function getAccessToken(oAuth2Client, callback) {
   const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
+    access_type: "offline",
     scope: SCOPES,
   });
-  console.log('Authorize this app by visiting this url:', authUrl);
+  console.log("Authorize this app by visiting this url:", authUrl);
   const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout,
   });
-  rl.question('Enter the code from that page here: ', (code) => {
+  rl.question("Enter the code from that page here: ", (code) => {
     rl.close();
     oAuth2Client.getToken(code, (err, token) => {
-      if (err) return console.error('Error retrieving access token', err);
+      if (err) return console.error("Error retrieving access token", err);
       oAuth2Client.setCredentials(token);
       // Store the token to disk for later program executions
       fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
         if (err) return console.error(err);
-        console.log('Token stored to', TOKEN_PATH);
+        console.log("Token stored to", TOKEN_PATH);
       });
       callback(oAuth2Client);
     });
@@ -73,24 +78,27 @@ function getAccessToken(oAuth2Client, callback) {
  * Lists the names and IDs of up to 10 files.
  * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
  */
-function listFiles(auth) {
-  const drive = google.drive({version: 'v3', auth});
-  drive.files.list({
-    pageSize: 10,
-    fields: 'nextPageToken, files(id, name)',
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const files = res.data.files;
-    if (files.length) {
-      console.log('Files:');
-      files.map((file) => {
-        console.log(`${file.name} (${file.id})`);
-      });
-    } else {
-      console.log('No files found.');
-    }
-  });
-}
+// function listFiles(auth) {
+//   const drive = google.drive({ version: "v3", auth });
+//   drive.files.list(
+//     {
+//       pageSize: 10,
+//       fields: "nextPageToken, files(id, name)",
+//     },
+//     (err, res) => {
+//       if (err) return console.log("The API returned an error: " + err);
+//       const files = res.data.files;
+//       if (files.length) {
+//         console.log("Files:");
+//         files.map((file) => {
+//           console.log(`${file.name} (${file.id})`);
+//         });
+//       } else {
+//         console.log("No files found.");
+//       }
+//     }
+//   );
+// }
 
 /*
 var fileId = config.fileID;
@@ -107,23 +115,23 @@ google.drive.file.export({
     })
     .pipe(dest);
     */
-   function downloadFile(auth) {
-    const drive = google.drive({version: 'v3', auth});
-    var fileId = config.fileID;
-    var dest = fs.createWriteStream('~/hsdiscordbot/formresponses.csv');
-    drive.files.export(
-      { fileId: fileId, mimeType: 'text/csv'},
-      { responseType: "stream" },
+function downloadFile(auth) {
+  const drive = google.drive({ version: "v3", auth });
+  var fileId = config.fileID;
+  var dest = fs.createWriteStream("test.csv");
+  drive.files.export(
+    { fileId: fileId, mimeType: "text/csv" },
+    { responseType: "stream" },
 
-     function (err, res) {
-       res.data
+    function (err, res) {
+      res.data
         .on("end", () => {
-          console.log('Done');
-    })
-    .on("error", function (err) {
-      console.log("Error during download", err);
-    })
-    .pipe(dest);
-  }
+          console.log("Done");
+        })
+        .on("error", function (err) {
+          console.log("Error during download", err);
+        })
+        .pipe(dest);
+    }
   );
 }
