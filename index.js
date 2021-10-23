a44730a72963a753882890f09b6337dc6426d05c
thomasdw33 <56744083+thomasdw33@users.noreply.github.com>
2021-10-23 17:48:06 -0400

    Update index.js

--- index.js
+++ index.js
@@ -184,14 +184,14 @@ bot.on("message", async message => {
             teamName = teamName.substring(0, 52);//makes sure it is less than 52 characters and above 3
 
             //Extract table number (in person only)
-            let indxTN = message.content.indexOf("[", indx2) + 1;
+            let indxTN = message.content.indexOf("[", indx2+ 1) ;
             let indxTN2 = message.content.indexOf("]", indxTN);
             let inPerson = !(indxTN < 0); //Determines if in person team
             var teamNumber;
             
             //Assigns teamNumber based on in person status
             if(inPerson){
-                let teamNumberString = message.content.substring(indxTN, indxTN2)
+                let teamNumberString = message.content.substring(indxTN+ 1, indxTN2)
                 console.log(teamNumberString);
                 teamNumber = parseInt(teamNumberString);
             } else {
